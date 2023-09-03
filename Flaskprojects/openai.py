d505a3c4c62389e5f882dcbd3ee5c681e5fce53b
Ankit Singh <114345412+AIWITHANKIT@users.noreply.github.com>
2023-09-04 04:16:38 +0530

    Update openai.py

--- Flaskprojects/openai.py
+++ Flaskprojects/openai.py
@@ -1,3 +1,10 @@
+import openai
+import json
+
+openai.api_key = ""
+
+
+
 # Example dummy function hard coded to return the same weather
 # In production, this could be your backend API or an external API
 def get_current_weather(location, unit="fahrenheit"):
