b66897f8f23d80948f46989355eb8e3d20d5290b
Ankit Singh <114345412+AIWITHANKIT@users.noreply.github.com>
2023-09-04 04:15:51 +0530

    Update openai.py

--- Flaskprojects/openai.py
+++ Flaskprojects/openai.py
@@ -1,1 +1,40 @@
+# Example dummy function hard coded to return the same weather
+# In production, this could be your backend API or an external API
+def get_current_weather(location, unit="fahrenheit"):
+    """Get the current weather in a given location"""
+    weather_info = {
+        "location": location,
+        "temperature": "72",
+        "unit": unit,
+        "forecast": ["sunny", "windy"],
+    }
+    return json.dumps(weather_info)
 
+
+# Step 1: send the conversation and available functions to GPT
+functions = [
+    {
+        "name": "get_current_weather",
+        "description": "Get the current weather in a given location",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "location": {
+                    "type": "string",
+                    "description": "The city and state, e.g. San Francisco, CA",
+                },
+                "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
+            },
+            "required": ["location"],
+        },
+    }
+]
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo-0613",
+    messages=[{"role": "user", "content": "What's the weather like in Boston?"}],
+    functions=functions,
+    function_call="auto",  # auto is default, but we'll be explicit
+)
+response_message = response["choices"][0]["message"]
+function_name = response_message["function_call"]["name"]
+print(function_name)
